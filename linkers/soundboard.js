d076e74863be738de0209f9b578696d39ac0ce21
wyrtz <spw-cool@hotmail.com>
2020-04-08 23:20:19 +0200

    now clears table before filling again. Play icon changes to stop icon, and clicking the same file again stops the playback. Bug: pressing this will not reset the icon (??)

--- linkers/soundboard.js
+++ linkers/soundboard.js
@@ -13,6 +13,7 @@ document.querySelector('#update').addEventListener('click', () => {
 })
 
 let tree
+const playIcon = "<i class='fa fa-play' />"
 
 function update_file_list() {
   const dirTree = require("directory-tree");
@@ -21,9 +22,9 @@ function update_file_list() {
   console.log(".wav files in " + dir)
   tree = dirTree(dir, { extensions: /\.wav/ })
   //console.log(tree)
-
-
   const table = document.querySelector("#mainTable");
+
+  $("#mainTable tr>td").remove(); //Clear table
 
   const children = tree.children
   children.forEach(element => {
@@ -32,12 +33,13 @@ function update_file_list() {
     const fileNameCell = row.insertCell(1)
     const shortcutCell = row.insertCell(2)
     const playCell = row.insertCell(3)
-    favoriteCell.innerHTML = "<i class='fa fa-star'>"
-    fileNameCell.innerHTML = element.name
-    shortcutCell.innerHTML = "Na"
-    playCell.innerHTML = "<i class='fa fa-play'></i>"
+    favoriteCell.innerHTML = "<i class='fa fa-star' />"
+    fileNameCell.innerHTML = element.name.split(".")[0]
+    shortcutCell.innerHTML = "-Na-"
+    playCell.innerHTML = playIcon
     row.addEventListener('click', () => {
-      play_sound(element.name);
+      play_sound(element.name, playCell);
+      playCell.innerHTML = "<i class='fa fa-stop' />"
     })
   });
   
@@ -45,16 +47,21 @@ function update_file_list() {
 
 let sound
 let keyboardInterrupt = 2
-let lastSoundFile
+let curRow
+let isPlaying = false
 
-function play_sound(soundFile) {
-  stop_playing()
-  if(lastSoundFile){
-    if(lastSoundFile === soundFile){
+function play_sound(soundFile, playCell) {
+  if(curRow){
+    console.log()
+    curRow.innerHTML = playIcon
+    if(isPlaying && curRow == playCell){
+      stop_playing()
       return
     }
   }
-  lastSoundFile = soundFile
+  curRow = playCell
+  stop_playing()
+  isPlaying = true
   const {PythonShell} = require("python-shell");
   const path =  require("path")
   let options = {
@@ -63,18 +70,22 @@ function play_sound(soundFile) {
     scriptPath : path.join(__dirname, '/file_player'),
     args: ['sound_files/' + soundFile]
   };
-
-
   sound = new PythonShell('play_file.py', options)
 
   sound.on("message", function(message) {
     console.log(message) //Print Python print statements
+    if(message === "Done"){
+      console.log("Got it bro!")
+      curRow.innerHTML = "<i class='fa fa-play' />"
+      isPlaying = false
+    }
   })
 }
 
 function stop_playing(){
   if(sound){
+    curRow.innerHTML = playIcon
     sound.terminate(keyboardInterrupt)
+    isPlaying = false
   }
 }
-
