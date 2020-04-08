cfee8555d81c802ebea4d31ae322e845c2e158f0
wyrtz <spw-cool@hotmail.com>
2020-04-08 20:21:14 +0200

    enabled adding .wav files and playing them

--- linkers/soundboard.js
+++ linkers/soundboard.js
@@ -15,26 +15,53 @@ document.querySelector('#update').addEventListener('click', () => {
 let tree
 
 function update_file_list() {
-  const dirTree = require("directory-tree")
-  console.log(__dirname)
+  const dirTree = require("directory-tree");
+  const path =  require("path");
+  const dir = path.join(__dirname, "sound_files")
   console.log(".wav files in " + dir)
   tree = dirTree(dir, { extensions: /\.wav/ })
-  console.log(tree)
+  //console.log(tree)
+
+
+  const table = document.querySelector("#mainTable");
+
+  const children = tree.children
+  children.forEach(element => {
+    const row = table.insertRow(-1)
+    const favoriteCell = row.insertCell(0)
+    const fileNameCell = row.insertCell(1)
+    const shortcutCell = row.insertCell(2)
+    const playCell = row.insertCell(3)
+    favoriteCell.innerHTML = "<i class='fa fa-star'>"
+    fileNameCell.innerHTML = element.name
+    shortcutCell.innerHTML = "Na"
+    playCell.innerHTML = "<i class='fa fa-play'></i>"
+    row.addEventListener('click', () => {
+      play_sound(element.name);
+    })
+  });
+  
 }
 
 let sound
 let keyboardInterrupt = 2
+let lastSoundFile
 
-function play_sound() {
+function play_sound(soundFile) {
   stop_playing()
+  if(lastSoundFile){
+    if(lastSoundFile === soundFile){
+      return
+    }
+  }
+  lastSoundFile = soundFile
   const {PythonShell} = require("python-shell");
   const path =  require("path")
-
   let options = {
     mode: 'text',
     pythonOptions: ['-u'], // get print results in real-time
     scriptPath : path.join(__dirname, '/file_player'),
-    args: ['file_player/ae.wav']
+    args: ['sound_files/' + soundFile]
   };
 
 
