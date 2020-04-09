bd318cec548cab41abc3c3eca71e165c044dbdde
wyrtz <spw-cool@hotmail.com>
2020-04-09 12:18:45 +0200

    added more soundfiles. Added sorting by pressing table header. Added (basic) support for folders

--- linkers/soundboard.js
+++ linkers/soundboard.js
@@ -18,22 +18,39 @@ $('#search').keyup(function() {
     }).hide();
 });
 
+// Cortesy of https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+// do the work...
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+  const table = th.closest('table');
+  const tbody = table.querySelector('tbody');
+  Array.from(tbody.querySelectorAll('tr'))
+    .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+    .forEach(tr => tbody.appendChild(tr) );
+})));
+
 document.querySelector('#stop').addEventListener('click', () => {
   stop_playing();
 })
 
 document.querySelector('#update').addEventListener('click', () => {
-  update_file_list();
+  update_file_list("");
 })
 
 let tree
 const playIcon = "<i class='fa fa-play' />"
 const stopIcon = "<i class='fa fa-stop' />"
 
-function update_file_list() {
+function update_file_list(lookup_dir) {
   const dirTree = require("directory-tree");
   const path =  require("path");
-  const dir = path.join(__dirname, "sound_files")
+  const dir = path.join(__dirname, "sound_files", lookup_dir)
   console.log(".wav files in " + dir)
   tree = dirTree(dir, { extensions: /\.wav/ })
   //console.log(tree)
@@ -43,20 +60,36 @@ function update_file_list() {
 
   const children = tree.children
   children.forEach(element => {
-    const row = table.insertRow(-1)
-    const favoriteCell = row.insertCell(0)
-    const fileNameCell = row.insertCell(1)
-    const shortcutCell = row.insertCell(2)
-    const playCell = row.insertCell(3)
-    favoriteCell.innerHTML = "<i class='fa fa-star' />"
-    fileNameCell.innerHTML = element.name.split(".")[0]
-    shortcutCell.innerHTML = "-Na-"
-    playCell.innerHTML = playIcon
-    row.addEventListener('click', () => {
-      play_sound(element.name, playCell);
-    })
+    createRow(table, element);
   });
   update_table_search()
+}
+
+function createRow(table, element) {
+  let direction = -1
+  if(element.type === "directory"){
+    direction = 0
+  }
+  const row = table.insertRow(direction);
+  const favoriteCell = row.insertCell(0);
+  const fileNameCell = row.insertCell(1);
+  const shortcutCell = row.insertCell(2);
+  const playCell = row.insertCell(3);
+  if(element.type === "directory"){
+    favoriteCell.innerHTML = "<i class='fa fa-folder' />"
+    fileNameCell.innerHTML = element.name
+    row.addEventListener('click', () => {
+      update_file_list(element.name);
+    })
+  }else{ //It is a file
+    favoriteCell.innerHTML = "<i class='fa fa-star' />";
+    fileNameCell.innerHTML = element.name.split(".")[0];
+    shortcutCell.innerHTML = "-Na-";
+    playCell.innerHTML = playIcon;
+    row.addEventListener('click', () => {
+      play_sound(element.path, playCell);
+    });
+  }
 }
 
 let sound
@@ -83,7 +116,7 @@ function play_sound(soundFile, playCell) {
     mode: 'text',
     pythonOptions: ['-u'], // get print results in real-time
     scriptPath : path.join(__dirname, '/file_player'),
-    args: ['sound_files/' + soundFile]
+    args: [soundFile]
   };
   sound = new PythonShell('play_file.py', options)
 
