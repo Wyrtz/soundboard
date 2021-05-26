938d1db34d90c8fd1cfc4980555c5117dc8502f4
wyrtz <spw-cool@hotmail.com>
2021-05-26 11:36:20 +0200

    cleared up in implementation of js Audio playback and fixed small bugs. standby for merge to master

--- js/jsSoundboard.js
+++ js/jsSoundboard.js
@@ -6,8 +6,6 @@
 
 'use strict';
 
-const {Howl, Howler} = require('howler');
-
 export const playIcon = "<i class='fa fa-play' />"
 const stopIcon = "<i class='fa fa-stop' />"
 let curRow
@@ -15,17 +13,14 @@ let sound
 let outputDeviceID = ""
 getSoundDevice()
 
-async function changeIcon(playCell){
-    playCell.innerHTML = playIcon
-}
-
+//Get the sounddevice to play the audio on
 async function getSoundDevice(){
     let soundDevices = await navigator.mediaDevices.enumerateDevices()
     let outputDevice = soundDevices.filter(element => element.label === "CABLE Input (VB-Audio Virtual Cable)")
     outputDeviceID = outputDevice[0].deviceId
 }
 
-//navigator.mediaDevices.enumerateDevices()
+//Play the given sound
 export async function play_sound(soundFile, playCell, fun){
     if(sound){
         curRow.innerHTML = playIcon
@@ -38,30 +33,15 @@ export async function play_sound(soundFile, playCell, fun){
     fun(soundFile)
     curRow = playCell
     await stop_playing()
-    playCell.innerHTML = stopIcon
-    /*sound = new Howl({
-        src:soundFile.path,
-        onend:function(){
-            changeIcon(playCell)
-        }
-    })*/
-    //console.log(navigator.mediaDevices.enumerateDevices())
     sound = new Audio(soundFile.path)
     if(outputDeviceID !== ""){
         sound.setSinkId(outputDeviceID)
     }
-    sound.autoplay = true
+    await sound.play()
+    playCell.innerHTML = stopIcon
+    sound.onended = () => {playCell.innerHTML = playIcon}
 }
 
-/*function playSound(buffer) {
-    var source = context.createBufferSource(); // creates a sound source
-    source.buffer = buffer;                    // tell the source which sound to play
-    source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-    source.start(0);                           // play the source now
-                                               // note: on older systems, may have to use deprecated noteOn(time);
-  }*/
-
-//  baseAudioContext.decodeAudioData
 //Say whatever text!
 export async function speak_text(text){
     var utterance = new SpeechSynthesisUtterance("");
@@ -70,6 +50,7 @@ export async function speak_text(text){
         window.speechSynthesis.speak(utterance);
 }
 
+//Stop whatever is playing atm.
 export async function stop_playing(){
     if(sound){
       curRow.innerHTML = playIcon
